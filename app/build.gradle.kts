9d00f0b8c9119389a3e9f6c7f4ba5f2936e1ab3b
Manjot <vvvt1316@leeds.ac.uk>
2026-03-06 23:39:36 +0000

    Setup database and Ktor

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,6 +32,15 @@ dependencies {
 
     // For database
     implementation("org.xerial:sqlite-jdbc:3.45.3.0")
+
+    // Ktor server
+    implementation("io.ktor:ktor-server-core:2.3.7")
+    implementation("io.ktor:ktor-server-netty:2.3.7")
+    
+    implementation("io.ktor:ktor-server-content-negotiation:2.3.7")
+    implementation("io.ktor:ktor-serialization-kotlinx-json:2.3.7")
+
+    implementation("ch.qos.logback:logback-classic:1.4.14")
 }
 
 // Apply a specific Java toolchain to ease working on different environments.
--- app/src/main/kotlin/org/example/App.kt
+++ app/src/main/kotlin/org/example/App.kt
@@ -1,4 +1,9 @@
 package org.example
+import io.ktor.server.engine.*
+import io.ktor.server.netty.*
+import io.ktor.server.application.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
 
 class App {
     val greeting: String
@@ -8,5 +13,16 @@ class App {
 }
 
 fun main() {
+    // Calls class to run seed into database schema
     DatabaseInitialiser.initialise()
+
+    // Runs Ktor server
+    embeddedServer(Netty, port = 8080) {
+    routing {
+        get("/") {
+            call.respondText("Ktor test")
+        }
+    }
+    }.start(wait = true)
+    
 }
